08b65b468c50b38451fc7ccb8535bbd63c0f3c13
Alexander <vsany@meta.ua>
2024-10-01 05:14:20 +0300

    [---] removed block 'if' which was for checking Connection status: close \ keep-alive [*] changed waiting delay from 120 seconds to 60 seconds [+] Added some logic to check is a site parking page or not

--- TelegramBotPooling.Api.Service/WebsiteHeadersHandler.cs
+++ TelegramBotPooling.Api.Service/WebsiteHeadersHandler.cs
@@ -25,76 +25,130 @@ public class WebsiteHeadersHandler : IWebsiteHeadersHandler
     {
         try
         {
-            var startTime = DateTime.Now;
-
             using var httpClientHandler = new HttpClientHandler();
-
             httpClientHandler.Proxy = new TorProxyService("127.0.0.1", 9150);
             httpClientHandler.UseProxy = true;
 
             using var torHttpClient = new HttpClient(httpClientHandler);
-            torHttpClient.Timeout = TimeSpan.FromSeconds(150);
+            torHttpClient.Timeout = TimeSpan.FromSeconds(60);
 
             var cts = new CancellationTokenSource();
-            var responseTask = torHttpClient.GetAsync(url, cts.Token);
-
-
-            var timeoutTask = Task.Delay(TimeSpan.FromSeconds(120), cts.Token);
+            var responseTask = torHttpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cts.Token);
+            var timeoutTask = Task.Delay(TimeSpan.FromSeconds(60), cts.Token);
             var completedTask = await Task.WhenAny(responseTask, timeoutTask);
 
             if (completedTask == timeoutTask)
             {
                 cts.Cancel();
-                _logger.LogCritical($"\n\n Site -------> {url} Timeout of 120 seconds elapsing. Returned false \n\n");
+                _logger.LogCritical($"Site -------> {url} Timeout of 60 seconds elapsing. Returned false.");
                 return false;
             }
 
             var response = await responseTask;
 
-            _logger.LogInformation($"\n\n Response time: {(DateTime.Now - startTime).TotalSeconds} seconds ");
-
+            // if (response.Headers.Location != null)
+            // {
+            //     var locationUrl = response.Headers.Location.ToString();
+            //
+            //     _logger.LogWarning($"Redirect detected to {locationUrl}");
+            //
+            //
+            //     if (locationUrl.Contains("lander", StringComparison.OrdinalIgnoreCase))
+            //     {
+            //         _logger.LogCritical($"Site: {url} redirected to {locationUrl}, identified as a parking page.");
+            //         return false;
+            //     }
+            // }
 
             if (response.StatusCode == HttpStatusCode.Forbidden)
             {
                 return true;
             }
-
             if (response.Content.Headers.ContentLength == 0 && response.StatusCode != HttpStatusCode.RedirectMethod)
             {
+                _logger.LogCritical($"Returned FALSE in if(response.Content.Headers.ContentLength == 0 && response.StatusCode != HttpStatusCode.RedirectMethod) \nSite: {url} ");
                 return false;
             }
 
-            if (response.Headers.TryGetValues("Connection", out var connectionValues))
+            var finalUri = response.RequestMessage?.RequestUri?.ToString();
+            
+
+            if (!string.IsNullOrEmpty(finalUri))
             {
-                var enumerable = connectionValues as string[] ?? connectionValues.ToArray();
-                if (enumerable.Contains("keep-alive"))
+                var uri = new Uri(finalUri);
+                // var path = uri.AbsolutePath;
+                var path = uri.AbsolutePath.TrimEnd('/'); 
+                
+                if (path.Equals("/leader", StringComparison.OrdinalIgnoreCase))
                 {
-                    return true;
-                }
-                if (enumerable.Contains("close"))
-                {
-                    _logger.LogCritical("Site: {Url} | Connection : close", url);
+                    _logger.LogCritical($"Final URL {finalUri} ends with '/leader', identified as a parking page.");
                     return false;
                 }
             }
 
-            return false;
+
+            var content = await response.Content.ReadAsStringAsync(cts.Token);
+            if (IsParkingPage(content))
+            {
+                _logger.LogCritical($"Site: {url} identified as a parking page based on content.");
+                return false;
+            }
+
+            return true;
         }
         catch (TaskCanceledException)
         {
-            _logger.LogCritical($"\n\n Site -------> {url} catch TaskCanceledException. Timeout of 120 seconds elapsing\n\n");
+            _logger.LogError($"Site -------> {url} TaskCanceledException. Timeout elapsing.");
             return false;
         }
         catch (HttpRequestException e)
         {
-            _logger.LogCritical($"\n\n Site -------> {url} catch HttpRequestException. {e.Message}\n\n");
+            _logger.LogWarning($"Site -------> {url} HttpRequestException: {e.Message}");
 
-            if (e.Message.Contains("The SSL connection could not be established, see inner exception."))
+            if (e.Message.Contains("The SSL connection could not be established"))
             {
                 return true;
             }
-            return false;
 
+            return false;
         }
+    }
+
+
+    private bool IsParkingPage(string content)
+    {
+        string[] parkingIndicators = {
+            "domain is for sale",
+            "related searches",
+            "this domain is parked",
+            "the domain is parked",
+            "buy this domain",
+            "domain name for sale",
+            "click here to make an offer",
+            "this domain may be for sale",
+            "under construction",
+            "this site is under construction",
+            "find available domain names",
+            "ads by google",
+            "sponsored listings",
+            "sponsored results",
+            "advertiser links",
+            "search results for",
+            "top searches",
+            "click here to continue",
+            "learn more about this domain",
+            "contact us for more information",
+            "the domain owner has not yet uploaded a website"
+        };
+
+        foreach (var indicator in parkingIndicators)
+        {
+            if (content.Contains(indicator, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
